2f0c8fef83647b236b146c5d18d44c031a6bce9c
Oleh Khalieiev <oleh.khalieiev@nure.ua>
2023-05-03 20:38:03 +0300

    Add structure project and model user

---
+++ lib/common/presentation/injector/injection_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InjectionContainer extends StatelessWidget {
+  const InjectionContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/common/presentation/navigation/route_names.dart
@@ -0,0 +1,6 @@
+class RouteNames {
+  static const String loginPage = '/login';
+  static const String registerPage = '/register';
+  static const String homePage = '/home';
+  static const String profilePage = '/profile';
+}
